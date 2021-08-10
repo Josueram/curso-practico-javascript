f95c7a8082696518fe15c54def5f9cd59ec67cfb
Josué Ramírez <josueramirez0110@gmail.com>
2021-08-09 19:37:30 -0500

    Se agrego un formulario para calcular el descuento

--- descuentos.js
+++ descuentos.js
@@ -1,16 +1,17 @@
-const precioOriginal = 120;
-const descuento = 18;
-
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajeConDescuento = 100 - descuento;
     const precioTotal = (precio * porcentajeConDescuento)/100;
     return precioTotal;
 }
 
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajeConDescuento,
-//     precioTotal,
-// })
+function precioConDescuento() {
+    const inputPrice = document.getElementById("InputPrice");
+    const inputDiscount = document.getElementById("InputDiscount");
 
+    const valuePrice = inputPrice.value;
+    const valueDiscount = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(valuePrice, valueDiscount);
+    const resultP = document.getElementById("resultPrice");
+    resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
